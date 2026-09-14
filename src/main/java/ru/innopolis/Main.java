18512b596370a274f201ee5eea64bcbf3d921e8f
Wal1yS <valera.tinyakov@gmail.com>
2026-09-14 20:22:53 +0300

    Testing functionality added

--- src/main/java/ru/innopolis/Main.java
+++ src/main/java/ru/innopolis/Main.java
@@ -1,17 +1,34 @@
 package ru.innopolis;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import ru.innopolis.lexer.Scanner;
+import ru.innopolis.lexer.Token;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "examples/14.imp";
+
+        try {
+            String sourceCode = Files.readString(Path.of(filePath));
+
+            System.out.println("Analyzing file: " + filePath);
+            System.out.println("--- SOURCE CODE ---");
+            System.out.println(sourceCode);
+            System.out.println("--- TOKENS ---");
+
+            Scanner scanner = new Scanner(sourceCode);
+            List<Token> tokens = scanner.scanTokens();
+
+            for (Token token : tokens) {
+                System.out.println(token.toString());
+            }
+
+        } catch (IOException e) {
+            System.err.println("Can't read file: " + e.getMessage());
         }
     }
 }
