9009ea968b34872de7171f1757fe3a3d72fe36ce
Wal1yS <valera.tinyakov@gmail.com>
2026-09-15 00:02:49 +0300

    Updated test set

--- src/main/java/ru/innopolis/Main.java
+++ src/main/java/ru/innopolis/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "examples/1.imp";
+        String filePath = "examples/14.txt";
 
         try {
             String sourceCode = Files.readString(Path.of(filePath));
